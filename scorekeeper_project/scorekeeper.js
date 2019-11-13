3a5b8b7dd9114f851173fd07e1301b445f2a7b27
shivam-prakash <prakashshivam9@gmail.com>
2019-11-13 21:52:49 +0530

    changes in css

--- scorekeeper_project/scorekeeper.js
+++ scorekeeper_project/scorekeeper.js
@@ -1,8 +1,10 @@
 var button1=document.getElementById("p1");
 var button2=document.getElementById("p2");
+var resetButton=document.getElementById("reset");
 var p1Score=0; var p2Score=0;
 var p1display=document.querySelector("#p1display");
 var p2display=document.querySelector("#p2display");
+var numinput=document.querySelector("input");
 var gameOver=false;
 var winningScore=5;
 
@@ -11,6 +13,7 @@ if(!gameOver){
 	p1Score++;
 	if(p1Score===winningScore){
 		gameOver=true;
+		p1display.classList.add("winner");
 	}
 }
 
@@ -21,7 +24,25 @@ button2.addEventListener("click",function(){
 	p2Score++;
 	if(p2Score===winningScore){
 		gameOver=true;
+		p2display.classList.add("winner");
 	}
 }
 p2display.textContent=p2Score;
+});
+resetButton.addEventListener("click",function(){
+	reset();
+
+});
+function reset(){
+p1display.textContent=p2display.textContent=0;
+	p1Score=p2Score=0;
+	p1display.classList.remove("winner");
+	p2display.classList.remove("winner");
+	gameOver=false;}
+
+numinput.addEventListener("change",function(){
+	var playto=document.getElementById("playto")
+	playto.textContent=Number(numinput.value);
+	winningScore=Number(numinput.value);
+	reset();
 });
